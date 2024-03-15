c3514cf356c5ab1789154eed03afe67b85d45f53
Zachary Elbring <elbringzach@gmail.com>
2024-03-15 17:27:19 -0500

    Added logic to database methods.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {console.error('putDb not implemented');
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const storage = transaction.objectStore('jate');
+  const req = storage.add({id: 1, value: content});
+  const res = await req;
+  console.log('Data saved successfully.', res);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.error('getDb not implemented');
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readonly');
+  const storage = transaction.objectStore('jate');
+  const req = storage.get(1);
+  const res = await req;
+  console.log('res.value', res);
+  return res?.value;
+};
 
 initdb();
